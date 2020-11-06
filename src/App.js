8614de0d60632f9a2282b10caecf0fbcbd865dbc
b4uwcoj <wahyudwi8514@gmail.com>
2020-11-06 17:52:58 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Courses from './containers/Courses/Courses';
 import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
+import NoMatch from './components/NoMatch/NoMatch';
 
 class App extends Component {
   render () {
@@ -32,6 +33,8 @@ class App extends Component {
             <Route path="/users" component={Users} />
             {/* <Route path="/courses/:courseId" component={Course} /> */}
             <Route path="/courses" component={Courses} />
+            <Redirect from="/all-courses" to="/courses" />
+            <Route component={NoMatch} />
           </Switch> 
       </div>
     );
